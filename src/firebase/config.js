a72904e981100167dbae30222331ab82fff6d8d8
ryuka-pc <r005mbkurino@gmail.com>
2022-03-14 17:22:50 +0900

    faistcomit

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDCgG7Ms2jSonqna-ttvBswHXUJanO-TOo",
+    authDomain: "chato-bot-demo.firebaseapp.com",
+    projectId: "chato-bot-demo",
+    storageBucket: "chato-bot-demo.appspot.com",
+    messagingSenderId: "471444829757",
+    appId: "1:471444829757:web:7563da8127e8af3815dc54",
+    measurementId: "G-59ZNCFDF96"
+};
+
+export default firebaseConfig
